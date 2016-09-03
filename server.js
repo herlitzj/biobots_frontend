6888d5b9b6a21bfb3ce65db9285b12f5913fe2db
Jeff Herlitz <herlitzj@gmail.com>
2016-09-03 17:16:17 -0400

    beginning of frontend

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var port = process.env.PORT || 3000;
+var request = require('request');
 var httpStatus = require('http-status-codes');
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
@@ -20,6 +21,16 @@ app.use('/admin', function(req, res) {
 app.use('/user', function(req, res) {
 	res.render('user');
 });
+
+app.get('/users/all', function(req, res) {
+	var baseURL = process.env.API_URL;
+	var url = baseURL + "/users/all";
+	request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the Google homepage.
+  }
+})
+})
 
 app.use('*', function(req, res) {
   res.status(httpStatus.NOT_FOUND).send('Invalid endpoint or REST action');

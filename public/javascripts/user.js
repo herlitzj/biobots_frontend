10952246bc8f6027541f6c1e74fa5323c252f407
Jeff Herlitz <herlitzj@gmail.com>
2016-09-03 17:11:32 -0400

    beginning of frontend

---
+++ public/javascripts/user.js
@@ -0,0 +1,45 @@
+d3.json("https://biobots-api.herokuapp.com/users/user5@gmail.com/prints", function(err, data) {
+    if(error) throw error;
+
+    var columns = [
+        { head: 'Live Percent', cl: 'title',
+          html: (row) => row.print_data.livePercent},
+        { head: 'Dead Percent', cl: 'center',
+          html: (row) => row.print_data.deadPercent},
+        { head: 'Elasticity', cl: 'center',
+          html: (row) => row.print_data.elasticity}
+    ];
+
+    // create table
+    var table = d3.select('body')
+        .append('table');
+
+    // create table header
+    table.append('thead').append('tr')
+        .selectAll('th')
+        .data(columns).enter()
+        .append('th')
+        .attr('class', (column) => column.cl)
+        .text((column) => column.head);
+
+    // create table body
+    table.append('tbody')
+        .selectAll('tr')
+        .data(data.all_prints).enter()
+        .append('tr')
+        .selectAll('td')
+        .data(function(row, i) {
+            return columns.map((c) => {
+                // compute cell values for this specific row
+                var cell = {};
+                d3.keys(c).forEach((k) => {
+                    cell[k] = typeof c[k] == 'function' ? c[k](row) : c[k];
+                });
+                return cell;
+            });
+        }).enter()
+        .append('td')
+        .html((column) => column.html)
+        .attr('class', (column) => column.cl);
+
+})
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var port = process.env.PORT || 3000;
+var httpStatus = require('http-status-codes');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+var app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use('/public', express.static(__dirname + '/public'));
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use('/admin', function(req, res) {
+	res.render('admin');
+});
+
+app.use('/user', function(req, res) {
+	res.render('user');
+});
+
+app.use('*', function(req, res) {
+  res.status(httpStatus.NOT_FOUND).send('Invalid endpoint or REST action');
+});
+
+console.log('Listening on port: ' + port);
+app.listen(port);
